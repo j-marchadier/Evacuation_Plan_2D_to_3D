776e6068b9effbc5d8458e005676054c93b22cff
Amst64 <91250466+Amst64@users.noreply.github.com>
2022-03-15 11:38:52 +0100

    Rotation de la camera avec la souris et materials

--- Unity_visu_3D/Camera.cs
+++ Unity_visu_3D/Camera.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+
 
 public class Camera : MonoBehaviour
 {
@@ -10,21 +12,35 @@ public class Camera : MonoBehaviour
     float inputX = 0;
     float inputY = 0;
 
+    Vector3 oldMousePos = Vector3.zero;
+    Vector3 currentMousePos = Vector3.zero;
+
     Vector3 direction;
-    
+
+    readfile rf;
+
+    private void Start()
+    {
+        rf = new readfile(Directory.GetFiles(Application.dataPath + "/", "*_mur.txt")[0], "walls");
+        rf.read();
+        transform.position = new Vector3(-rf.meanX, 0, rf.meanZ);
+        transform.rotation = Quaternion.identity;
+    }
     // Update is called once per frame
     void Update()
     {
         inputX = Input.GetAxisRaw("Horizontal");
         inputY = Input.GetAxisRaw("Vertical");
+        currentMousePos = Input.mousePosition;
 
         direction = new Vector3(0, 0, inputY);
 
         transform.Translate(direction * speed * Time.deltaTime);
 
-        if(inputX != 0)
-        {
-            transform.rotation = Quaternion.AngleAxis(Time.deltaTime * speed * inputX, transform.up) * transform.rotation;
-        }
+        float mouseX = currentMousePos.x - oldMousePos.x;
+
+        transform.rotation = Quaternion.AngleAxis(Time.deltaTime * speed * mouseX, transform.up) * transform.rotation;
+
+        oldMousePos = currentMousePos;
     }
 }
