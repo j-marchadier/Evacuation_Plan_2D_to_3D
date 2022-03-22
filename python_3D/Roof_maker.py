14592754e2458f6370ac15ccd8da2a88c81f617d
Aleaume Landrea <51741094+landreaa@users.noreply.github.com>
2022-03-22 16:14:11 +0100

    ,

--- python_3D/Roof_maker.py
+++ python_3D/Roof_maker.py
@@ -1,3 +1,4 @@
+import math
 import sys
 from cv2 import sqrt
 import numpy as np
@@ -14,7 +15,9 @@ def arrange_name(filename, extension):
 
 
 def point_distance(p1, p2):
-    return sqrt(((p1[0] - p2[0]) ^ 2) + ((p1[1] - p2[1]) ^ 2))
+    calc = math.sqrt(math.pow((p1[0] - p2[0]), 2) +
+                     math.pow((p1[1] - p2[1]), 2))
+    return calc
 
 
 list_of_points = {}
@@ -32,10 +35,49 @@ def make_adjacency_matrix(mat):
             list_of_points[key_counter] = (line[2], line[3])
             key_counter = key_counter + 1
 
+    n = max(list_of_points.keys())
+    mat_adj = [[0]*n] * n
+
+    list_p_x = [i[0] for i in list(list_of_points.values())]
+    list_p_z = [i[1] for i in list(list_of_points.values())]
+
+    for i in range(n):
+        point_actuel = list_of_points[i]
+        for j in range(n):
+            point_regarde = list_of_points[j]
+            if point_actuel[0] == point_regarde[0] or point_actuel[0] == point_regarde[1] or point_actuel[1] == point_regarde[0] or point_actuel[1] == point_regarde[1]:
+                # s'il y a des connexions entre ces points.
+                mat_adj[i][j] = mat_adj[i][j] + 1
+
+    return mat_adj
+
 
 def printall(d):
     for i in d:
         print(i, d[i])
+
+
+def make_triangles(mat_adj):
+    triangles = []
+    for i in range(len(mat_adj)):
+        for j in range(i+1, len(mat_adj)):
+            if (mat_adj[i][j] == 1):
+                for k in range(j+1, len(mat_adj)):
+                    if mat_adj[i][k] == 1 and mat_adj[j][k] == 1:
+
+                        ptlist = list(
+                            (list_of_points[i], list_of_points[j], list_of_points[k]))
+                        mini = min(ptlist)
+                        ptlist.remove(mini)
+                        maxi = max(ptlist)
+                        ptlist.remove(maxi)
+                        midi = ptlist[0]
+                        tup = (mini, midi, maxi)
+                        if tup not in triangles:
+                            triangles.append(tup)
+                        mat_adj[j][k] = 0
+
+    return triangles
 
 
 def main():
@@ -45,32 +87,21 @@ def main():
     filename = arrange_name(filename, ".txt")
 
     array_in = np.genfromtxt(filename, delimiter=';')
+
     temp = np.reshape(array_in.flatten(), (-1, 2))
     x = [temp[i][0] for i in range(temp.shape[0])]
     z = [temp[i][1] for i in range(temp.shape[0])]
 
-    xz = zip(x, z)
-    all_points = [(a, b) for a, b in temp if abs(a-b) >= 10]
-    x, z = zip(*all_points)
+    for i in range(len(x)):
+        for j in range(len(z)):
+            if(point_distance((x[i], z[i]), (x[j], z[j])) <= 10):
+                temp[j][0] = x[i]
+                temp[j][1] = z[i]
 
-    # make_adjacency_matrix(array_in)
-    # printall(list_of_points)
-    # print(all_points)
-    matrix = [[0 for i in range(int(max(z)+1))]
-              for j in range(int(max(x)+1))]
-
-    for i in range(len(all_points)):
-        matrix[int(all_points[i][0])][int(all_points[i][1])] = 255
-
-    for i in range(len(matrix)):
-        str = ""
-        for j in range(len(matrix[0])):
-            str = str + "{} ".format(matrix[i][j])
-        # print(str)
-
-    fig, ax = plt.subplots()
-    ax.matshow(matrix, cmap=plt.cm.Blues)
-    plt.show()
+    redone_mat = np.reshape(array_in.flatten(), (-1, 4))
+    matadj = make_adjacency_matrix(redone_mat)
+    triangles = make_triangles(matadj)
+    print(triangles)
 
     with open('exit.txt', 'w') as f:
         f.write('Create a new text file!')
